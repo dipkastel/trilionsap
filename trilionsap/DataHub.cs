2b57be7f40e0d74dfa7d9da3f1d7316c7e6f6845
amir.ashrafi <dipkastel@gmail.com>
2021-05-01 20:41:36 +0430

    start the project

---
+++ trilionsap/DataHub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace trilionsap
+{
+    public class DataHub : Hub
+    {
+        public void SendToAll(string var1)
+        {
+            Clients.All.broadcastMessage(var1);
+        }
+        public void SendToAll(string var1, string var2)
+        {
+            Clients.All.broadcastMessage(var1, var2);
+        }
+        public void SendToAll(string var1, string var2,string var3)
+        {
+            Clients.All.broadcastMessage(var1, var2,var3);
+        }
+    }
+}
---
+++ trilionsap/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(trilionsap.Startup))]
+
+namespace trilionsap
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
